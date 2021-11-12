520717ba8d3748d597ce1f695b6fb7be9ce712d9
Valery Efremov <valeriy131100@gmail.com>
2021-11-12 18:32:08 +0300

    Remove .decode and .encode because of Python 3

--- add_note2journal.py
+++ add_note2journal.py
@@ -55,9 +55,9 @@ if __name__ == '__main__':
     print(json.dumps(context, ensure_ascii=False, indent=4))
 
     new_note = noteStore.copyNote(config.JOURNAL_TEMPLATE_NOTE_GUID, config.JOURNAL_NOTEBOOK_GUID)
-    utitle_without_comment = new_note.title.decode('utf8').split('#', 1)[0]
+    utitle_without_comment = new_note.title.split('#', 1)[0]
     utitle = utitle_without_comment.strip().format(**context)
-    new_note.title = utitle.encode('utf8')
+    new_note.title = utitle
     noteStore.updateNote(new_note)
     
     print(u'Note created: %s' % utitle)
